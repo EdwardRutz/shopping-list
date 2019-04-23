aedbe4ac18f6a46b8bca33b5efc54ef4850eeb31
edward-ca <edward-ca@opayq.com>
2019-04-23 11:37:16 -0400

    MVP Done

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 
 >A demo of Python basics with a console app to build a shopping list
 
+## Dependencies
+
+- Python
 
 
+## User Stories
 
-
-
-
-
+- As a user, I should be able to add an item to the list
+- As a user, Contually be prompted to add an item to list if needed
+- User, Provide help to user when called for
+- User, keep track of the length of the list
+- User, state when done, print out entire list
+- User, be able to look at the list anytime and verify order
 
 
 ## Sources
 
 - [Python Lists](https://teamtreehouse.com/library/introducing-lists)
+- [Python.org](https://www.python.org/)
 
--- shopping-list.py
+++ shopping-list.py
@@ -1,22 +1,38 @@
-# User, Provide help to user when called for
+shopping_list = []
+
+def add_to_list(item):    
+    shopping_list.append(item)
+    # Notify user the item was added and give length of list
+    print("Item added: {}".format(new_item))
+    print("Total items in list:  {}".format(len(shopping_list)))
+    
 def show_help():
     print("What groceries to get at the store?")
     print("""
+    Enter 'SHOW' to see entire list.
     Enter 'DONE' to exit app.
     Enter 'HELP" for help menu.
     """)
+    
+def show_list():
+    print("Here is your list: ")
+    for item in shopping_list:
+        print(item)
 
 show_help()
-
-# As a user, Contually be prompted to add an item to list if needed
 while True:
-    new_item = input("> ")
+    new_item = input("Add item to list: ")
     
-    #User, state when done, print out entire list
     if new_item == 'DONE':
         break
     elif new_item == 'HELP':
         show_help()
         continue
-        
+    elif new_item == 'SHOW':
+        show_list()
+        continue
+         
+    add_to_list(new_item)
+
+show_list()
         
